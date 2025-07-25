f85078ba870a2231fd78c8191e67bd4cbffb1e64
grisonb <79510016+grisonb@users.noreply.github.com>
2025-07-25 18:51:41 +0200

    v1.0.js

--- script.js
+++ script.js
@@ -420,7 +420,7 @@ const SearchToggleControl = L.Control.extend({
         this.toggleButton.href = '#';
         this.communeDisplay = L.DomUtil.create('div', 'commune-display-control', topBar);
         const versionDisplay = L.DomUtil.create('div', 'version-display', mainContainer);
-        versionDisplay.innerText = 'v1.1 beta'; // <<<=== VERSION MISE À JOUR ICI ===>>>
+        versionDisplay.innerText = 'v1.0'; // <<<=== VERSION MISE À JOUR ICI ===>>>
         L.DomEvent.disableClickPropagation(mainContainer);
         L.DomEvent.on(this.toggleButton, 'click', L.DomEvent.stop);
         L.DomEvent.on(this.toggleButton, 'click', () => {
