1e0d62665b851ea2882ca7e3c594bd3d30f71ac4
grisonb <79510016+grisonb@users.noreply.github.com>
2025-08-13 09:45:50 +0200

    v2.1.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const APP_CACHE_NAME = 'communes-app-cache-v200'; // Version 2.0
-const DATA_CACHE_NAME = 'communes-data-cache-v200';
-const TILE_CACHE_NAME = 'communes-tile-cache-v200';
+const APP_CACHE_NAME = 'communes-app-cache-v201'; // Version 2.1
+const DATA_CACHE_NAME = 'communes-data-cache-v201';
+const TILE_CACHE_NAME = 'communes-tile-cache-v201';
 
 const APP_SHELL_URLS = [
     './',
